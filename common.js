53ebbfab8bc4b61356479314c59837ec43add1d8
rhit-hans <ZHANGZ36@rose-hulman.edu>
2026-09-24 23:15:28 -0400

    Add navbar and footer to common.js

---
+++ common.js
@@ -0,0 +1,15 @@
+const navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="resume.html">Resume</a>
+        <a href="portfolio.html">Portfolio</a>
+    </nav>`;
+
+const footer = document.getElementById("footer");
+
+footer.innerHTML = `<footer>
+        <p>© 2026 Hans Zhang</p>
+        <a href="https://www.linkedin.com/">LinkedIn</a>
+    </footer>`;
